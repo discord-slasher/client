999d02d5851884d62c9999e658ca489fecfc3fbf
SynysterZV <greendayfanj@gmail.com>
2021-08-13 03:10:48 -0400

    Forgot to build

--- dist/types/Command.d.ts
+++ dist/types/Command.d.ts
@@ -1,6 +1,13 @@
-import { ChatInputApplicationCommandData, ApplicationCommandPermissionData, CommandInteraction } from "discord.js";
-export interface Command extends ChatInputApplicationCommandData {
+import { MessageApplicationCommandData, ChatInputApplicationCommandData, ApplicationCommandPermissionData, CommandInteraction } from "discord.js";
+interface SlashCommand extends ChatInputApplicationCommandData {
     ownerOnly?: boolean;
     permissions?: ApplicationCommandPermissionData[];
     exec(interaction: CommandInteraction): Promise<void>;
 }
+interface MessageContextMenu extends MessageApplicationCommandData {
+    ownerOnly?: boolean;
+    permissions?: ApplicationCommandPermissionData[];
+    exec(interaction: CommandInteraction): Promise<void>;
+}
+export declare type Command = SlashCommand | MessageContextMenu;
+export {};
