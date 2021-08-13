5161979fac2fb351482887dc3c225d0f44e95f93
SynysterZV <greendayfanj@gmail.com>
2021-08-13 03:07:45 -0400

    Changed Command type

--- src/types/Command.ts
+++ src/types/Command.ts
@@ -1,7 +1,15 @@
-import { ChatInputApplicationCommandData, ApplicationCommandPermissionData, CommandInteraction } from "discord.js";
+import { MessageApplicationCommandData ,ChatInputApplicationCommandData, ApplicationCommandPermissionData, CommandInteraction } from "discord.js";
 
-export interface Command extends ChatInputApplicationCommandData {
+interface SlashCommand extends ChatInputApplicationCommandData {
     ownerOnly?: boolean;
     permissions?: ApplicationCommandPermissionData[];
     exec(interaction: CommandInteraction): Promise<void>;
 }
+
+interface MessageContextMenu extends MessageApplicationCommandData {
+    ownerOnly?: boolean;
+    permissions?: ApplicationCommandPermissionData[];
+    exec(interaction: CommandInteraction): Promise<void>;
+}
+
+export type Command = SlashCommand | MessageContextMenu
